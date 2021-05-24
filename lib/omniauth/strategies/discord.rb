a4df25a20c3d2f3a04633d569371579343c2a455
Ernest Bursa <eb@tonik.pl>
2021-05-24 21:55:09 +0200

    Define guild_id and disable_guild_select option

--- lib/omniauth/strategies/discord.rb
+++ lib/omniauth/strategies/discord.rb
@@ -12,7 +12,7 @@ module OmniAuth
              authorize_url: 'oauth2/authorize',
              token_url: 'oauth2/token'
 
-      option :authorize_options, %i[scope permissions prompt]
+      option :authorize_options, %i[scope permissions prompt guild_id disable_guild_select]
 
       uid { raw_info['id'] }
 
