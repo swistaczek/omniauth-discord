38cf641d5bc323885d05082b12d9fd9821753395
gracz21 <kamil.walkowiak@fourthwall.com>
2021-08-04 12:44:57 +0200

    Add basic guild info to the base auth response

--- lib/omniauth/strategies/discord.rb
+++ lib/omniauth/strategies/discord.rb
@@ -20,7 +20,12 @@ module OmniAuth
         {
           name: raw_info['username'],
           email: raw_info['verified'] ? raw_info['email'] : nil,
-          image: "https://cdn.discordapp.com/avatars/#{raw_info['id']}/#{raw_info['avatar']}"
+          image: "https://cdn.discordapp.com/avatars/#{raw_info['id']}/#{raw_info['avatar']}",
+          guild: {
+            id: raw_guild_info['id'],
+            name: raw_guild_info['name'],
+            roles: raw_guild_info['roles']
+          }
         }
       end
 
@@ -32,6 +37,10 @@ module OmniAuth
 
       def raw_info
         @raw_info ||= access_token.get('users/@me').parsed
+      end
+
+      def raw_guild_info
+        @raw_guild_info ||= access_token.params['guild']
       end
 
       def callback_url
